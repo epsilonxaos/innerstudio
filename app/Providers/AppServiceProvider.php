a13854d08618e0948e3306c8be1793834fc715b3
Jesus Gonzalez <wilberthg16@gmail.com>
2022-01-31 21:25:57 -0600

    Se cambio en el AppProvider cuando este en produccion o no

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -25,11 +26,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        if(App::environment('production')) {
 
-        $this->app->bind('path.public', function() {
-            return base_path('../public_html/');
-        });
-        Schema::defaultStringLength(191);
+            URL::forceScheme('https');
+
+            $this->app->bind('path.public', function() {
+                return base_path('../public_html/');
+            });
+            Schema::defaultStringLength(191);
+        }
     }
 }
