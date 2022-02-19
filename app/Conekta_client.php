5b9d0ff3c83853d4c7697841dfa85cbd4ef13df0
jorge <jmworking92@gmail.com>
2022-02-19 00:23:33 -0600

    fix en la clase conekta client para el test del wh

--- app/Conekta_client.php
+++ app/Conekta_client.php
@@ -1,22 +1,28 @@
 <?php
 namespace App;
 require_once("../lib/Conekta.php");
-
+use Conekta\Charge;
+use Conekta\Conekta;
+use Conekta\Customer as ConektaCustomer;
+use Conekta\Handler;
+use Conekta\Order;
+use Conekta\ParameterValidationError;
+use Conekta\ProcessingError;
 
 class Conekta_client
 {
     function __construct() {
-        \Conekta\Conekta::setApiKey(config('services.pagos.skey'));
-        \Conekta\Conekta::setApiVersion("2.0.0");
+        Conekta::setApiKey(config('services.pagos.skey'));
+        Conekta::setApiVersion("2.0.0");
     }
 
     public function newOrder($data)
     {
-        return \Conekta\Order::create($data);
+        return Order::create($data);
     }
 
     public function newClient($data)
     {
-        return \Conekta\Customer::create($data);
+        return Customer::create($data);
     }
 }
