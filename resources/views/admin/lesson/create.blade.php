0f4208127ed0a92d2346db363ba7137bb08e4926
Jesus Glez <jesusgleztr94@gmail.com>
2022-03-07 18:20:02 -0600

    Add clases ajuste

--- resources/views/admin/lesson/create.blade.php
+++ resources/views/admin/lesson/create.blade.php
@@ -68,6 +68,7 @@
                             </button>
                         </div>
                     @endif
+
                     <div class="card overflow-unset">
                         <div class="card-content">
                             <form method="post" action="{{route('admin.lesson.insert')}}">
@@ -81,7 +82,10 @@
                                                 <select name="id_instructor" id="id_instructor" class="browser-default @error('id_instructor') invalid @enderror">
                                                     <option value="" selected>Instructores</option>
                                                     @foreach($instructores as $in)
-                                                        <option {{ session('id_instructor') ? session('id_instructor') == $in['id_instructor'] ? 'selected' : '' : old('id_instructor') == $in['id_instructor'] ? 'selected' : '' }} value="{{ $in['id_instructor'] }}">{{$in['name']}}</option>
+                                                        <option {{ session('id_instructor') ? 
+                                                            (session('id_instructor') == $in['id_instructor'] ? 'selected' : '')
+                                                            :
+                                                            (old('id_instructor') == $in['id_instructor'] ? 'selected' : '') }} value="{{ $in['id_instructor'] }}">{{$in['name']}}</option>
                                                     @endforeach
 
                                                 </select>
@@ -90,14 +94,12 @@
                                                 <label>Elije un tipo</label>
                                                 <select name="tipo" id="tipo" class="browser-default @error('tipo') invalid @enderror">
                                                     <option value="" selected>Tipo</option>
-                                                    <option {{ old('tipo') ? old('tipo') == 'classic' ? 'selected' : '' : old('tipo') == 'classic' ? 'selected' : '' }} value="classic">Classic</option>
-                                                    <option {{ old('tipo') ? old('tipo') == 'power' ? 'selected' : '' : old('tipo') == 'power' ? 'selected' : '' }} value="power">Power</option>
-                                                    <option {{ old('tipo') ? old('tipo') == 'interval' ? 'selected' : '' : old('tipo') == 'interval' ? 'selected' : '' }} value="interval">Interval</option>
-                                                    <option {{ old('tipo') ? old('tipo') == 'sculpt' ? 'selected' : '' : old('tipo') == 'sculpt' ? 'selected' : '' }} value="sculpt">Sculpt</option>
-                                                    <option {{ old('tipo') ? old('tipo') == 'full body flow' ? 'selected' : '' : old('tipo') == 'full body flow' ? 'selected' : '' }} value="full body flow">Full Body Flow</option>
-                                                    <option {{ old('tipo') ? old('tipo') == 'yoga' ? 'selected' : '' : old('tipo') == 'yoga' ? 'selected' : '' }} value="yoga">Yoga</option>
-
-
+                                                    <option {{(old('tipo') == 'classic') ? 'selected' : '' }}  value="classic">Classic</option>
+                                                    <option {{(old('tipo') == 'power') ? 'selected' : '' }}  value="power">Power</option>
+                                                    <option {{(old('tipo') == 'interval') ? 'selected' : '' }}  value="interval">Interval</option>
+                                                    <option {{(old('tipo') == 'sculpt') ? 'selected' : '' }}  value="sculpt">Sculpt</option>
+                                                    <option {{(old('tipo') == 'full body flow') ? 'selected' : '' }}  value="full body flow">Full Body Flow</option>
+                                                    <option {{(old('tipo') == 'yoga') ? 'selected' : '' }}  value="yoga">Yoga</option>
                                                 </select>
                                             </div>
                                             <div class="input-field col s12 m4">
